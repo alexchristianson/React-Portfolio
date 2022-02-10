8b9ba6d551053c44fcff5b7cc24719fc53b584ee
Alex C <alex.christianson.05@gmail.com>
2022-02-10 14:33:32 -0600

    trying to fix deployment

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Router basename='/react-portfolio'>
+      <Router>
         <Header />
         <Switch>
           <Route 
